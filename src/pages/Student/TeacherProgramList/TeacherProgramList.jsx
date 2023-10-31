9e931ba0570db6f61512a81839fa3e479c3f2626
VoThanhAn28 <85780063+VoThanhAn28@users.noreply.github.com>
2023-10-31 22:37:46 +0700

    home student

---
+++ src/pages/Student/TeacherProgramList/TeacherProgramList.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import "./TeacherProgramList.css"
+import SchoolIcon from '@mui/icons-material/School';
+
+
+function TeacherProgramList() {
+    return ( 
+        <Box sx={{
+            height: '200vh',
+        }}>
+            <Box sx={{height: "130px", marginTop: "90px", borderRadius:"5px", marginLeft: "1%", marginRight:"1%" ,backgroundColor:"#E2D6D6"}}>
+                <Typography sx={{fontSize: "40px", marginLeft: "2%", fontFamily:"cursive", paddingBottom: "20px"}}>Tên giáo viên - Tên môn</Typography>
+                <Typography sx={{fontSize: "25px", marginLeft: "2%", fontFamily:"cursive"}}>Tab chính/Môn/Giáo viên</Typography>
+            </Box>
+            <Box sx={{ border: '1px solid #ccc', p: 2, marginLeft:"1%", marginRight:"1%" }} >
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+
+                <Box sx={{height:"185px", borderRadius: "5px", backgroundColor:"#BFBDBD", marginTop: "1%"}} >
+                    <Typography sx={{fontSize:"35px", fontFamily:"cursive", marginLeft:"2%", paddingTop:"1%"}}>Tên chương</Typography>
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+
+                    <Box sx={{display:"flex", alignItems:"center"}}>
+                        <SchoolIcon sx={{fontSize:"25px", marginLeft: "4%"}}/>
+                        <Typography sx={{fontSize:"25px", marginLeft: "1%"}}> Tên bài học</Typography>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+     );
+}
+
+export default TeacherProgramList;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,10 @@
 import DefaultLayout from '../components/Layout/DefaultLayout/DefaultLayout';
 import LoginLayout from '../components/Layout/LoginLayout/LoginLayout';
+import LayoutStudent from "../components/Layout/LayoutStudent/LayoutStudent";
 import ChangePassword from "../pages/Logins/ChangePassword/ChangePassword"
 import ForgotPassword from '../pages/Logins/ForgotPassword/ForgotPassword';
-import Home from "../pages/Guest/Home/Home"
+import Home from "../pages/Guest/Home/Home";
+import HomeStudent from "../pages/Student/Home/HomeStudent";
 import LoginPage from '../pages//Logins/Login/LoginPage';
 import ProfileStaff from '../pages/Profile/ProfileStaff/ProfilePage';
 import ProfileStudent from '../pages/Profile/ProfileStudent/ProfileStudent';
@@ -12,20 +14,24 @@ import BookTutorPage from "../pages/BookTutor/BookTutorPage";
 import SubjectPage from "../pages/ListSubject/SubjectPage";
 import PhysicsExperiment from "../pages/PhysicsExperiment/PhysicsExperiment";
 import ListTutor from '../pages/ListTutor/ListTutorPage';
+import TeacherProgramList from '../pages/Student/TeacherProgramList/TeacherProgramList';
 
 const publicRoute = [
   { path: '/', component: Home, layout: DefaultLayout },
-  { path: '/login', component: LoginPage, layout: LoginLayout },
   { path: '/signup', component: SignUp, layout: LoginLayout },
+  { path: '/login', component: LoginPage, layout: LoginLayout },
+  { path: '/subject', component: SubjectPage, layout: DefaultLayout},
+  { path: '/listtutor', component: ListTutor, layout: DefaultLayout},
+  { path: '/booktutor', component: BookTutorPage, layout: DefaultLayout},
+  { path: '/homestudent', component: HomeStudent, layout: LayoutStudent },
   { path: '/forgotpass', component: ForgotPassword, layout: LoginLayout },
   { path: '/changepass', component: ChangePassword, layout: LoginLayout },
+  { path: '/physics' ,component: PhysicsExperiment, layout: DefaultLayout},
   { path: '/profile-staff', component: ProfileStaff, layout: DefaultLayout },
   { path: '/profile-student', component: ProfileStudent, layout: DefaultLayout },
   { path: '/profile-teacher', component: ProfileTeacher, layout: DefaultLayout },
-  { path: '/booktutor', component: BookTutorPage, layout: DefaultLayout},
-  { path: '/subject', component: SubjectPage, layout: DefaultLayout},
-  { path: '/physics' ,component: PhysicsExperiment, layout: DefaultLayout},
-  { path: '/listtutor', component: ListTutor, layout: DefaultLayout},
+  { path: '/teacherprogramlist', component: TeacherProgramList, layout: LayoutStudent },
+
 ];
 const privateRoute = [];
 
