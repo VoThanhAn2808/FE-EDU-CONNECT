c6e8f800e6b1fefcdaef85f3094d332a6ed592a3
hiuonly <hieuntde150078@fpt.edu.vn>
2023-11-05 20:32:27 +0700

    update

--- src/pages/Guest/ListSubject/SubjectPage.jsx
+++ src/pages/Guest/ListSubject/SubjectPage.jsx
@@ -1,46 +1,50 @@
-import React from "react";
-import './../ListSubject/Subject.css';
+import React, { useEffect, useState } from "react";
+import "./../ListSubject/Subject.css";
 import { Box, Grid, Typography } from "@mui/material";
-import dcmdxdn from "../../../assests/subject.png";
-import PersonIcon from '@mui/icons-material/Person';
-import { Button } from '@mui/material';
+import PersonIcon from "@mui/icons-material/Person";
+import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
-
-
-const data = [
-    { id: 1, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 2, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 3, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 4, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 5, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 6, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 7, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 8, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 9, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 10, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 11, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-    { id: 12, name: 'Đại số 10', number: 100, imgLink: dcmdxdn },
-];
+import axios from "axios";
 
 function SubjectPage() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8081/course/findCourseByClass?classcourseid=1")
+            .then((response) => {
+                setData(response.data);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <Box className="body">
             <Grid container spacing={2}>
                 {data.map((item) => (
-                    <Grid item xs={12} sm={6} md={5} lg={3} key={item.id}>
-                        <Box className='container'>
-                            <img src={item.imgLink} alt={item.name} className="subject-img" />
-                            <Typography className="nameSubject">{item.name}</Typography>
-                            <Box sx={{ display: 'flex' }} >
+                    <Grid item xs={12} sm={6} md={5} lg={3} key={item.classCourseId}>
+                        <Box className="container">
+                            <img src={item.img} alt={item.courseName} className="subject-img" />
+                            <Typography className="nameSubject">
+                                {item.courseName} {item.className}
+                            </Typography>
+                            <Box sx={{ display: "flex" }}>
                                 <Typography className="inforsubject">
                                     <PersonIcon className="total" />
-                                    {item.number}</Typography>
-                                <Link to='/listtutor'>
-                                    <Button variant="contained" color="primary" 
+                                    {item.CountStudent}
+                                </Typography>
+                                <Link to="/listtutor">
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
                                         sx={{
-                                            height: '20px',
-                                            width: '80px'
-                                        }}>
+                                            height: "20px",
+                                            width: "80px",
+                                        }}
+                                    >
                                         Chi tiết
                                     </Button>
                                 </Link>
@@ -49,7 +53,7 @@ function SubjectPage() {
                     </Grid>
                 ))}
             </Grid>
-        </Box >
+        </Box>
     );
 }
 
