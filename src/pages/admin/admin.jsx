5e2c4f61478e80530d3a1e22b9dabcb3ccccb158
hiuonly <hieuntde150078@fpt.edu.vn>
2023-11-16 02:51:08 +0700

    update

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,13 +1,39 @@
 import { Box } from "@mui/material";
+import { Chart } from 'react-google-charts';
 
 function Admin() {
-    return ( 
+    const data = [
+        ['Month', 'Sales', 'Expenses', 'Profit'],
+        ['Jan', 1000, 400, 600],
+        ['Feb', 1170, 460, 710],
+        ['Mar', 660, 1120, 380],
+        ['Apr', 1030, 540, 490],
+        ['May', 800, 600, 200],
+      ];
+
+      const options = {
+        title: 'Sales, Expenses, and Profit',
+        hAxis: {
+          title: 'Month',
+        },
+        vAxis: {
+          title: 'Amount',
+        },
+        seriesType: 'bars',
+        series: { 2: { type: 'line' } },
+      };
+    return (
         <Box>
-            <Box>
-                
-            </Box>
+            <Chart
+                width={'100%'}
+                height={'400px'}
+                chartType="ComboChart"
+                loader={<Box>Loading Chart</Box>}
+                data={data}
+                options={options}
+            />
         </Box>
-     );
+    );
 }
 
 export default Admin;
