9aee7c74027468eeb0348bca7392040aaab861dd
Thai Hoa <Hoatde150209@fpt.edu.vn>
2023-12-06 02:17:04 +0700

    ManagerStudentPage

--- src/pages/Tutor/ManagerStudent/ManagerStudentPage.jsx
+++ src/pages/Tutor/ManagerStudent/ManagerStudentPage.jsx
@@ -4,24 +4,25 @@ import React, { useEffect, useState } from "react";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import avt from './../../../assests/giasu.jpg'
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { jwtDecode } from 'jwt-decode';
+
 function ManagerStudent() {
-
     const [open, setOpen] = useState(false);
-
-    const handleOpen = () => setOpen(true);
+    const [studentid, setStudentId] = useState(null);
     const handleClose = () => setOpen(false);
-
     const [anchorElUser, setAnchorElUser] = useState(null);
-
     const [listStudentfinished, setListStudentfinished] = useState([]);
     const decodedToken = jwtDecode(localStorage.getItem('token'));
     const [page, setPage] = useState(1);
     const [status, setStatus] = useState(0);
-
+    const [pageCount, setPageCount] = useState();
     const [currentDate, setCurrentDate] = useState(new Date());
+    const [responseDataDetail, setResponseDataDetail] = useState([]);
+    const { courseId } = useParams();
+
+    console.log('Course ID from URL:', courseId);
 
     useEffect(() => {
         // Update the current date every second
@@ -32,20 +33,32 @@ function ManagerStudent() {
         // Clear the interval on component unmount
         return () => clearInterval(intervalId);
     }, []);
+
     useEffect(() => {
-        axios.get(`http://localhost:8081/educonnect/tutor/studentfinished?tutorid=${decodedToken.id}&page=${page}&status=${status}`)
+        axios.get(`http://localhost:8081/educonnect/tutor/studentfinished?tutorid=${decodedToken.id}&page=${page}&status=${status}&courseid=${courseId}`)
             .then((response) => {
                 setListStudentfinished(response.data);
-                console.log(response.data);
             })
             .catch((error) => {
                 console.log(error);
             })
-    }, [page, status]);
+    }, [page, status, courseId]);
+    useEffect(() => {
+        axios.get(`http://localhost:8081/educonnect/countstudent?tutorid=${decodedToken.id}&status=${status}&courseid=${courseId}`)
+            .then((response) => {
+                setPageCount(response.data);
+                console.log('Count page: ' + response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }, [status]);
 
 
-    const handleOpenUserMenu = (event) => {
+    const handleOpenUserMenu = (event, studentId) => {
         setAnchorElUser(event.currentTarget);
+        setStudentId(studentId); // Use the state callback
+        console.log('Student ID:', studentId);
     };
 
     const handleCloseUserMenu = () => {
@@ -61,7 +74,25 @@ function ManagerStudent() {
             return "Đợi Học";
         }
     };
+    const handleOpen = () => {
+        axios.get(`http://localhost:8081/educonnect/tutor/student/viewprofile/timeline?tutorid=${decodedToken.id}&studentid=${studentid}&courseid=${courseId}`)
+            .then((response) => {
+                setResponseDataDetail(response.data)
+                setOpen(true);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
+
+    const fillDetailStudent = responseDataDetail.map((item) => ({
+        lesson: item.lesson,
+        timeline: item.timeline,
+    }));
+    const resultArray = fillDetailStudent.map((item) => `${item.lesson}(${item.timeline})`);
+    const resultString = resultArray.join(', ');
 
     const style = {
         position: 'absolute',
@@ -118,7 +149,7 @@ function ManagerStudent() {
                                         <TableCell sx={{ width: '20%', height: '50px', fontSize: '15px', fontFamily: 'cursive' }}>{checkStatus((new Date(item.startdate)).getTime(), (new Date(item.enddate)).getTime())}</TableCell>
                                         <TableCell sx={{ width: '20%', height: '50px', fontSize: '15px', fontFamily: 'cursive' }}><img src={'http://localhost:8081/edu/file/fileImg/' + item.studentid + '/' + item.img} alt={`Discount Image for ${item.title}`} style={{ maxWidth: '100%', maxHeight: '100%', }} /></TableCell>
                                         <TableCell sx={{ height: '50px', textAlign: 'center' }}>
-                                            <MoreHorizIcon onClick={handleOpenUserMenu} sx={{ fontSize: '30px' }} />
+                                            <MoreHorizIcon onClick={(event) => handleOpenUserMenu(event, item.studentid)} sx={{ fontSize: '30px' }} />
                                         </TableCell>
                                     </TableRow>
                                 )) :
@@ -156,17 +187,24 @@ function ManagerStudent() {
                     >
                         <Box sx={style}>
                             <Box sx={{ marginTop: '-100px' }}>
-                                <Avatar src={avt} sx={{ width: '90px', height: '90px', marginLeft: '90px', marginBottom: '20px' }} />
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Học sinh: Nguyễn Văn A - 00921</Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Ngày Sinh: 2001-02-02</Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>SĐT: 0987654321</Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Email: email@gmail.com</Typography>
-                                <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Thời gian học: Thứ 2(07:00), Thứ 3(08:00), Thứ 4(9:00)</Typography>
+                                {responseDataDetail.length > 0 ?
+                                    (<>
+                                        <Avatar src={'http://localhost:8081/edu/file/fileImg/' + responseDataDetail[0].studentid + '/' + responseDataDetail[0].img} sx={{ width: '90px', height: '90px', marginLeft: '90px', marginBottom: '20px' }} />
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Học sinh: {responseDataDetail[0].fullname}</Typography>
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Ngày Sinh: {responseDataDetail[0].birthdate}</Typography>
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>SĐT: {responseDataDetail[0].phone}</Typography>
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Email: {responseDataDetail[0].email}</Typography>
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>Thời gian học: {resultString}</Typography>
+                                    </>) : (
+                                        <Typography sx={{ fontSize: '15px', fontFamily: 'cursive', marginLeft: '12%' }}>
+                                            Không có thông tin học sinh
+                                        </Typography>
+                                    )}
                             </Box>
                         </Box>
                     </Modal>
                     <Box sx={{ marginBottom: '50px', display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
-                        <Pagination count={10} sx={{ '& .MuiPaginationItem-root': { fontSize: '15px', minWidth: '50px' } }} />
+                        <Pagination count={pageCount} sx={{ '& .MuiPaginationItem-root': { fontSize: '15px', minWidth: '50px' } }} />
                     </Box>
                 </Box>
             </Box>
