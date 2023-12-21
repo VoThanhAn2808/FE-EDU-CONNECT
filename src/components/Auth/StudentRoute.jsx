8b1a481c6e8a2a615f935e667a35dfc310614217
Thai Hoa <Hoatde150209@fpt.edu.vn>
2023-12-21 11:14:04 +0700

    Clear source

--- src/components/Auth/StudentRoute.jsx
+++
@@ -1,7 +0,0 @@
-import {decodeToken, getRole} from "../../utils/commons"
-import PageNotFound from "../../pages/PageNotFound";
-const StudentRoute = ({ element, allowedRoles }) => {
-  const { isLogin, role } = decodeToken()
-  return isLogin && allowedRoles.includes(getRole(role))  ? element : <PageNotFound/>
-};
-export default StudentRoute;
